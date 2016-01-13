31d20e453d988ba175904aab5134372a631dec07
André de Jager <andrethehunter@gmail.com>
2016-01-13 12:17:41 +1000

    fixed consecutive duplicate lines test

--- test/simulation.test.js
+++ test/simulation.test.js
@@ -3,14 +3,30 @@ var expect = require('chai').expect
 var simulation = require('../lib/simulation')
 
 describe('simulation', function () {
-	it('return "Invalid Date" if the date is invalid', function () {
+
+	it('returns "Invalid Date" if the date is invalid', function () {
 		expect(simulation(new Date('2016-01-14T09:05:00+100'))).to.equal('Invalid Date')
 	})
 
-	it('return no duplicate lines', function () {
-		var lines = simulation(new Date()).split('\n')
+	it('return no consecutive duplicate lines', function () {
+		var lines = simulation(new Date('2016-01-14T09:05:00+1000'))
+			.split('\n')
+			.map(function (line) {
+				//remove the date part of the line
+				return line.split(' - ')[1]
+			})
+
 		expect(lines).to.not.be.empty
-		expect(new Set(lines).size).to.equal(lines.length)
+		//test for consecutive duplicate lines
+		expect(lines.every((function() {
+			var prev
+			return function(line) {
+				if (line === prev)
+					return false
+				prev = line
+				return true
+			}
+		})())).to.be.true
 	})
 
 })
